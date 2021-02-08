b47a51c40832e7d26d5bf83ec34efda863dba1d8
shima-kherad <78704352+shima-kherad@users.noreply.github.com>
2021-02-08 18:40:33 +0330

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def main():
-  # print("Keep it logically awesome.")
+  print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes)
 
+__name__ == "__main__"  
 if __name__== "__main__":
   main()
